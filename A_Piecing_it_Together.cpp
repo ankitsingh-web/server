178ee90cc7e1a40189f34ee067dbebde35ee9a8c
My project <singhamit18033@gmail.com>
2025-04-02 12:46:02 +0530

    mongo db url

---
+++ A_Piecing_it_Together.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double a = 89.000001, b = 92.000012;
+  cout << a << " " << b << endl;
+  return 0;
+}
--- config/bdconnection.js
+++ config/bdconnection.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 mongoose.set ('strictQuery',false);
 const connectionToDB=async()=>{
   try{
-    console.log(process.env.MONGO_URL)
-  const {connection}=await mongoose.connect(process.env.MONGO_URL||'mongodb://localhost:27017/lms')
+  
+  const {connection}=await mongoose.connect(process.env.MONGO_URL)
 
 if(connection){
   console.log(`connect to MongoDB "${connection.host}"`);
 }}
 catch(e){
-  console.log(e);
+  console.error(e);
   process.exit(1);//all the process kill out
 }
 }
